5543868dfebffd8b0f30bb20884b66b087c72610
Kazuto <mail@kazuto.de>
2021-10-23 13:24:18 +0200

    fix: workflows

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,10 +2,17 @@
 
 use Kazuto\PhpCsPreset\Preset;
 
+$classLoader = require __DIR__ . '/vendor/autoload.php';
+$classLoader->register(true);
+
+// Override rules to your liking
+$rules = [
+];
+
 $finder = PhpCsFixer\Finder::create()
     ->in([
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
 
-return Preset::apply($finder);
+return Preset::apply($finder, $rules);
